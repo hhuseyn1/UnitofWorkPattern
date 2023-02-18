d2a06b1738d985cf14ca1a5a5cc04713e2562a57
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-02-18 16:17:41 +0400

    Add AppDbContext

---
+++ UnitofWorkPattern/Contexts/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using UnitofWorkPattern.Models;
+
+namespace UnitofWorkPattern.Contexts;
+
+public class LibraryDbContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+        var connectionString = configuration.GetConnectionString("LibraryDb");
+
+        optionsBuilder.UseSqlServer(connectionString);
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Press> Presses { get; set; }
+    public DbSet<Theme> Themes { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<Lib> Libs { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Faculty> Faculties { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<S_Card> S_Cards { get; set; }
+    public DbSet<T_Card> T_Cards { get; set; }
+}
